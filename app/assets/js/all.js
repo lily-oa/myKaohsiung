2bcbd199d63e5d197616792cecb4b8df6deb9c47
lily <a0976198586@gmail.com>
2022-07-11 12:08:20 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -6,3 +6,23 @@ const subtitle = document.querySelector('.subtitle');        //卡片標題
 const cardList = document.querySelector('.card-list');       //卡片
 const pageList = document.querySelector('.pagination-list'); // 分頁
 
+let data = [];
+let page = {};
+
+// 渲染 select filter
+function renderData(showData) {
+  // 渲染有存在的行政區到選單欄位
+  let selectGroup = showData.map(item => item.Zone);
+  let newSelect = selectGroup.filter((item, index) => selectGroup.indexOf(item) == index);
+  let selectStr = `<option value="高雄全區" selected class="fs-7">-- 高雄全區 --</option>`;
+  newSelect.forEach(function(item){
+    let list=`<option value="${item}">${item}</option>`;
+    selectStr += list;
+  });
+  select.innerHTML = selectStr;
+  subtitle.textContent = '高雄全區';
+
+  pagination(showData, 1);
+}
+
+// 渲染所有清單資料到畫面上
