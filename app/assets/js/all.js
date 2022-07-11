df67a4d55b6db23a7e3a43221705115db1b527b5
lily <a0976198586@gmail.com>
2022-07-11 10:07:57 +0800

    update

---
+++ app/assets/js/all.js
@@ -0,0 +1,8 @@
+
+const url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json'; //高旅網  api
+const select = document.querySelector('.select-group');      //行政區
+const tabsList = document.querySelector('.tabs-list');       //熱門區
+const subtitle = document.querySelector('.subtitle');        //卡片標題
+const cardList = document.querySelector('.card-list');       //卡片
+const pageList = document.querySelector('.pagination-list'); // 分頁
+
