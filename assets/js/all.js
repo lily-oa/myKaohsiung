9cde7778b47b1a310d95a8dfc60181dbab4e8e9b
lily <a0976198586@gmail.com>
2022-07-11 10:08:18 +0800

    Update 2022-07-11T02:08:17.124Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,14 @@
 "use strict";
+
+var url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json'; //高旅網  api
+
+var select = document.querySelector('.select-group'); //行政區
+
+var tabsList = document.querySelector('.tabs-list'); //熱門區
+
+var subtitle = document.querySelector('.subtitle'); //卡片標題
+
+var cardList = document.querySelector('.card-list'); //卡片
+
+var pageList = document.querySelector('.pagination-list'); // 分頁
 //# sourceMappingURL=all.js.map
