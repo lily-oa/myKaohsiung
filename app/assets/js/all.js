87e19fc978eb8ae37b3d09a0fa4d5314300b0569
lily <a0976198586@gmail.com>
2022-07-28 07:31:29 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -28,4 +28,12 @@ function renderData(showData) {
 // 渲染所有清單資料到畫面上
 
 //-----------------------------------------------------------------暫時新學習需用到的語法再寫專案
-console.log('1234');
+// https://hexschool.github.io/ajaxHomework/data.json
+axios.get('https://hexschool.github.io/ajaxHomework/data.json')  //抓取這個網址的資料並回傳回來
+  .then(function(response){        //抓取的資料回傳後才會觸發then裡的函式
+    let ary = response.data;       // 將抓到資料暫存在 ary，這是我的資料集
+    console.log(ary[0].name);      // 我只想抓它的名字
+    const title = document.querySelector('.title'); // 指定範圍
+    console.log(title);
+    title.innerHTML = ary[0].name; //將資料寫入網頁的指定範圍
+  });
