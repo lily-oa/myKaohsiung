dc38dfbdc478da261942b9fe6e66d95c38d85f95
lily <a0976198586@gmail.com>
2022-08-11 03:07:38 +0800

    Update 2022-08-10T19:07:36.527Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -45,7 +45,7 @@ send.addEventListener('click', function (e) {
 });
 
 function callSingUp() {
-  if (account.value == '' || password == '') {
+  if (account.value == '' || password.value == '') {
     alert('此空格不可為空，請填寫正確資料');
     return;
   }
@@ -55,7 +55,14 @@ function callSingUp() {
   obj.password = password.value;
   console.log(obj);
   axios.post('https://hexschool-tutorial.herokuapp.com/api/signup', obj).then(function (response) {
-    console.log(response.data);
+    if (response.data.message == '帳號註冊成功') {
+      alert('恭喜註冊成功');
+    } else {
+      alert('帳號註冊失敗');
+    }
+
+    account.value = '';
+    password.value = '';
   })["catch"](function (error) {
     console.log(error);
   });
