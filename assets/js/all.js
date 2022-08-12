b8dbd064d4b4db367a54fec8ed3bb6aaade47030
lily <a0976198586@gmail.com>
2022-08-12 20:19:26 +0800

    Update 2022-08-12T12:19:24.037Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -79,24 +79,12 @@ sendSignIn.addEventListener('click', function (e) {
 });
 
 function callSignIn() {
-  if (accountSignIn.value == '' || passwordSignIn.value == '') {
-    alert('請寫好你的東西!');
-    return;
-  }
-
   var obj2 = {};
   obj2.email = accountSignIn.value;
   obj2.password = passwordSignIn.value;
   console.log(obj2);
-  axios.post('https://hexschool-tutorial.herokuapp.com/api/signin', obj2).then(function (response) {
-    if (response.data.message == '登入成功') {
-      alert('恭喜你登入成功');
-    } else {
-      alert('登入失敗');
-    }
-
-    accountSignIn.value = '';
-    passwordSignIn.value = '';
+  axious.post('https://hexschool-tutorial.herokuapp.com/api/signin').then(function (response) {
+    console.log(response.data);
   })["catch"](function (error) {
     console.log(error);
   });
