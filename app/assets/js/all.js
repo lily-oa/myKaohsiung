bb208729a5f84dbf974f81b35900c77452e6c13c
lily <a0976198586@gmail.com>
2022-08-16 07:00:21 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,8 @@
-
 const url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json'; //高旅網  api
-const select = document.querySelector('.select-group');      //行政區
-const tabsList = document.querySelector('.tabs-list');       //熱門區
-const subtitle = document.querySelector('.subtitle');        //卡片標題
-const cardList = document.querySelector('.card-list');       //卡片
+const select = document.querySelector('.select-group'); //行政區
+const tabsList = document.querySelector('.tabs-list'); //熱門區
+const subtitle = document.querySelector('.subtitle'); //卡片標題
+const cardList = document.querySelector('.card-list'); //卡片
 const pageList = document.querySelector('.pagination-list'); // 分頁
 
 let data = [];
@@ -29,25 +28,24 @@ function renderData(showData) {
 
 //-----------------------------------------------------------------暫時新學習需用到的語法再寫專案
 // 實作 toDoList 練習
-let myData = [
-  {
-    "content":"·待辦事項一" 
+let myData = [{
+    "content": "·待辦事項一"
   },
   {
-    "content":"·今天記得刷牙"
+    "content": "·今天記得刷牙"
   },
   {
-    "content":"·記得去吃飯"
+    "content": "·記得去吃飯"
   }
 ]
 
 //寫一個函式把裡面的處理方式都包起來
-function myRenderData(){
+function myRenderData() {
   let str = '';
-  myData.forEach(function(item){
+  myData.forEach(function (item) {
     str += `
-    <li class="ms-2">${item.content}<input type="button" value="刪除代辨" class="m-2"></li>
-    `
+  <li class="ms-2">${item.content}<input type="button" value="刪除代辨" class="m-2"></li>
+  `
   });
   const list = document.querySelector('.list');
   list.innerHTML = str;
