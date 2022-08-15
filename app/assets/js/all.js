602faa24cbf0a3def6d7efcd0612bce86eace7a0
lily <a0976198586@gmail.com>
2022-08-15 22:17:06 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -29,3 +29,24 @@ function renderData(showData) {
 
 //-----------------------------------------------------------------暫時新學習需用到的語法再寫專案
 // 實作 toDoList 練習
+let myData = [
+  {
+    "content":"待辦事項一" 
+  },
+  {
+    "content":"今天記得刷牙"
+  }
+]
+
+//寫一個函式把裡面的處理方式都包起來
+let str = '';
+myData.forEach(function(item){
+  str+=`
+  <li class="mb-2">${item.content}<input type="button" value="刪除代辦" class="delete ms-2"></li>
+  `
+});
+
+console.log(str);
+
+const list = document.querySelector('.list');
+list.innerHTML = str;
