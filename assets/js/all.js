2132f27f8752fec4a7e497c29f2912b34268fb10
lily <a0976198586@gmail.com>
2022-08-15 22:24:56 +0800

    Update 2022-08-15T14:24:54.047Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -42,11 +42,15 @@ var myData = [{
   "content": "今天記得刷牙"
 }]; //寫一個函式把裡面的處理方式都包起來
 
-var str = '';
-myData.forEach(function (item) {
-  str += "\n  <li class=\"mb-2\">".concat(item.content, "<input type=\"button\" value=\"\u522A\u9664\u4EE3\u8FA6\" class=\"delete ms-2\"></li>\n  ");
-});
-console.log(str);
-var list = document.querySelector('.list');
-list.innerHTML = str;
+function myRenderData() {
+  var str = '';
+  myData.forEach(function (item) {
+    str += "\n  <li class=\"mb-2\">".concat(item.content, "<input type=\"button\" value=\"\u522A\u9664\u4EE3\u8FA6\" class=\"delete ms-2\"></li>\n  ");
+  });
+  var list = document.querySelector('.list');
+  list.innerHTML = str;
+} // 一開始預設時會先跑一次
+
+
+myRenderData();
 //# sourceMappingURL=all.js.map
