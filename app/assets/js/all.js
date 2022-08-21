920f47d164c0b787f8a9e6af6a951c4b22014102
lily <a0976198586@gmail.com>
2022-08-21 12:05:33 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -27,46 +27,4 @@ function renderData(showData) {
 // 渲染所有清單資料到畫面上
 
 //-----------------------------------------------------------------暫時新學習需用到的語法再寫專案
-// 實作 toDoList 練習
-let myData = [];
-const txt = document.querySelector('.txt');
-const save = document.querySelector('.save');
-const list = document.querySelector('.list');
-
-// 寫一個函式把裡面的處理方式都包起來
-// 狀態初始化用的
-function myRenderData(){
-  let str = '';
-  myData.forEach(function(item, index){
-    str += `
-    <li class="m-2 fs-4">·${item.content}<input type="button" value="刪除" data-num="${index}" class="delete m-2"></li>
-    `
-  });
-  const list = document.querySelector('.list');
-  list.innerHTML = str;
-  txt.value = '';
-}
-
-// myRender();
-//新增代辦功能
-save.addEventListener('click',function(e){
-  if(txt.value ==''){
-    alert('此欄位不可為空');
-    return;
-  }
-  let myObj = {};
-  myObj.content = txt.value;
-  myData.push(myObj);
-  myRenderData();
-});
-
-// 刪除代辦功能
-list.addEventListener('click', function(e){
-  if(e.target.getAttribute('class')!=='delete m-2'){
-    alert('你沒有點到正確的按鈕');
-    return;
-  }
-  let num = e.target.getAttribute('data-num');
-  myData.splice(num, 1);
-  myRenderData();
-});
+// 陣列操作 map
