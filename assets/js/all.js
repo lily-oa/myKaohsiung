610f92ea95027afd89f1f1ed5cdd9f8f64972e1c
lily <a0976198586@gmail.com>
2022-08-26 06:59:19 +0800

    Update 2022-08-25T22:59:17.416Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -33,22 +33,17 @@ function renderData(showData) {
   pagination(showData, 1);
 } // 渲染所有清單資料到畫面上
 //-----------------------------------------------------------------暫時新學習需用到的語法再寫專案
-// 箭頭函式，縮寫寫法
-// 如果函式搭配 return 
-// 如果只有一個參數，可以省略大括號
-// 沒有參數，還是要有空括號
+// 陣列操作 map、 filter
 
 
-var numA = function numA(x) {
-  return x * x;
-}; //return 可以省略
+var myData = [1, 8, 13, 20];
+var newData = myData.map(function (item) {
+  return item + 2;
+});
+console.log(newData); // 簡寫成箭頭函式
 
-
-console.log(numA(3));
-
-var numB = function numB() {
-  return "\u6578\u5B57\u76F8\u4E58".concat(9);
-};
-
-console.log(numB());
+var newData2 = myData.map(function (item) {
+  return item + 3;
+});
+console.log(newData2);
 //# sourceMappingURL=all.js.map
