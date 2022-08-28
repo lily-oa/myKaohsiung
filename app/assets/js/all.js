0ebadbb883194e98297ee2388fe65cdc11e1e05b
lily <a0976198586@gmail.com>
2022-08-28 18:15:31 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,11 +2,22 @@ const url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datast
 const select = document.querySelector('.select-group'); //行政區
 const tabsList = document.querySelector('.tabs-list'); //熱門區
 const subtitle = document.querySelector('.subtitle'); //卡片標題
-const cardList = document.querySelector('.card-list'); //卡片
+const cardList = document.querySelector('.card-list'); //卡片  ul
 const pageList = document.querySelector('.pagination-list'); // 分頁
 
 let data = [];
 let page = {};
+
+// axios get data
+function getData(){
+  axios.get(url)
+  .then(function(response){
+    data = response.data.result.records;
+    console.log(response.data);
+    renderData(data);
+  });
+}
+getData();
 
 // 渲染 select filter
 function renderData(showData) {
@@ -21,7 +32,7 @@ function renderData(showData) {
   select.innerHTML = selectStr;
   subtitle.textContent = '高雄全區';
 
-  pagination(showData, 1);
+  // pagination(showData, 1);
 }
 
 // 渲染所有清單資料到畫面上
