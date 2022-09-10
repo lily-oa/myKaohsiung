9f68b0fa7f97ebac022dd59c13458565b402611f
lily <a0976198586@gmail.com>
2022-09-10 11:30:03 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,9 @@
+AOS.init({
+  once: true,
+  offset: 50,
+});
+AOS.refresh();
+
 const url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json'; //高旅網  api
 const select = document.querySelector('.select-group'); //行政區
 const tabsList = document.querySelector('.tabs-list'); //熱門區
